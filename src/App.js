71228013c105e12fd91ed4693430b2c5ef675d09
TulioCaz <tuliocaz.windcoast@gmail.com>
2020-03-11 18:37:28 -0300

    feat: created the Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Header from './components/Header/Header';
 
 class App extends Component {
   render() {
-    return <h1>Hello World!</h1>
+    return <Header />
   };
 };
 
